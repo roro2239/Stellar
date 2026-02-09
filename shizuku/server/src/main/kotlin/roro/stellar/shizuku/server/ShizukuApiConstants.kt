10b1acb7727f7f0ff1c61bf596e4bf8cf9ffcc0b
ItosEO <m2265514616@163.com>
2026-02-09 21:38:56 +0800

    [shizuku-server] 修复了由于没有认真处理合并冲突导致的无法编译问题

--- shizuku/server/src/main/kotlin/roro/stellar/shizuku/server/ShizukuApiConstants.kt
+++ shizuku/server/src/main/kotlin/roro/stellar/shizuku/server/ShizukuApiConstants.kt
@@ -5,6 +5,8 @@ object ShizukuApiConstants {
     const val SERVER_VERSION = 13
     const val SERVER_PATCH_VERSION = 5
 
+    const val SHIZUKU_APP_PACKAGE_NAME = "moe.shizuku.privileged.api"
+
     const val BINDER_DESCRIPTOR = "moe.shizuku.server.IShizukuService"
 
     const val BINDER_TRANSACTION_transact = 1599295570 // '_' << 24 | 'S' << 16 | 'H' << 8 | 'K'
